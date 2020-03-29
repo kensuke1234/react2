089199f7346302dd4b553887dee0f247b4ccabc6
Kensuke Machii <kensukemachii@gmail.com>
2020-03-29 21:33:02 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import { Table } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   Container,
@@ -11,40 +12,73 @@ import {
 
 function App() {
   const [value, setValue] = useState("");
-
-  const [todos, setTodo] = useState([]);
-
-  const addTodo = text => {
   
-  const newTodo = [...todos, text];
+  const [todos, SetTodo] = useState([]);
+  
+  const addTodo = text => {
 
-  setTodo(newTodo);
-  console.log(newTodo);
-};
+    const newTodo = [...todos, text];
+
+    SetTodo(newTodo);
+    console.log(newTodo);
+  
+  };
 
   const handleSubmit = e => {
     e.preventDefault();
 
     console.log(value);
-    addTodo(value);
-};
+    addTodo(value); //ここでaddTodoを実行するとValueにTextが入り、todosの配列の中にどんどん追加。
+  };
+
+  const removeTodo = index => {
+    const newTodo = [...todos];
+    newTodo.splice(index, 1);
+    SetTodo(newTodo);
+  };
+
   return (
     <div className="App">
      <Container>
-    <h1 className="mt-4">Todo リスト</h1>
+    <h1 className="mt-4">人生でやり遂げたいリスト100</h1>
     
-    <Form>
+    <Form onSubmit={handleSubmit}> 
       <InputGroup>
-      <Input value={value} 
-      onCharge={e => setValue(e.target.value)}
+      <Input 
+      value={value} 
+      onChange = {e => setValue(e.target.value)}
       />
 
       <InputGroupAddon addonType="append">
-      <Button type="submit" color="primary">追加</Button>
+      <Button type="submit" color="primary">目標を決める</Button>
      </InputGroupAddon>
      </InputGroup>
      </Form>
      </Container>
+
+    {/* リスト表示のロジック */}
+    <Container>
+      <Table>
+        <tbody>
+          {/* 論理演算子をつかつてTodosがある時のみに処理を実行}*/}
+          {/*mapは新しく処理したのちに新しい配列を返してくれる */}
+          {todos &&
+           todos.map((todo, index) => (
+          <tr>
+            <tr key={index}>
+            <th>{todo}</th>
+            <td>
+               <Button color="danger" onClick={() => removeTodo(index)}>
+              達成
+              </Button>
+      </td>
+      </tr>
+          </tr>
+           ))}
+        </tbody>
+       </Table>
+    </Container>      
+
     </div>
   );
 }
